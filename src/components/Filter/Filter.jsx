ae604fc1da8e2207c085880e0dcd554aa73b9523
AloBordo1992 <122152325+AloBordo1992@users.noreply.github.com>
2023-07-16 13:44:30 +0200

    code cleanup

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types';
-// import css from './Filter.module.css';
 
 export function Filter(props) {
   return (
@@ -13,7 +12,6 @@ export function Filter(props) {
           onChange={event => props.onFilterChange(event.currentTarget.value)}
         />
       </label>
-      {/* <button type="submit">sdfsf</button> */}
     </div>
   );
 }
@@ -22,35 +20,3 @@ Filter.propTypes = {
   onFilterChange: PropTypes.func.isRequired,
   value: PropTypes.string.isRequired,
 };
-
-// import { Component } from 'react';
-// import PropTypes from 'prop-types';
-// import css from './Filter.module.css';
-
-// export class Filter extends Component {
-//   static propTypes = {
-//     onFilterChange: PropTypes.func.isRequired,
-//     value: PropTypes.string.isRequired,
-//   };
-
-//   filterChangeHandler = event => {
-//     this.props.onFilterChange(event.currentTarget.value);
-//   };
-
-//   render() {
-//     return (
-//       <div className={css.filter}>
-//         <label>
-//           Find contacts by name <br />
-//           <input
-//             type="text"
-//             name="name"
-//             value={this.props.value}
-//             onChange={this.filterChangeHandler}
-//           />
-//         </label>
-//         {/* <button type="submit">sdfsf</button> */}
-//       </div>
-//     );
-//   }
-// }
